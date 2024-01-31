c30c4627dc6342119344463374f4ff80500055b5
unknown <kayv@hotmail.fr>
2024-01-31 06:52:53 +0100

    components pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React from 'react';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Skills from '../components/Skills';
+import '../styles/home.css';
 
 export default function Home() {
 	return (
 		<section className="home">
-		oui
+			<Header />
+			<Hero />
+			<Skills />
 		</section>
 	);
 }
