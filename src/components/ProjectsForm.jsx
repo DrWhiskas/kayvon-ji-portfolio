45952c41fa0057d0224f892ae8fb2872d9f9eb39
unknown <kayv@hotmail.fr>
2024-02-02 07:43:40 +0100

    projects form

--- src/components/ProjectsForm.jsx
+++ src/components/ProjectsForm.jsx
@@ -1,12 +1,40 @@
 import React from "react";
+import { FaGithub, FaDesktop } from 'react-icons/fa';
+import '../styles/projectsForm.css'
 
-export default function ProjectsForm({img, title, description, skills, linkGit, linkPage}){
+export default function ProjectsForm({ emoji, title, description, skills, linkGit, linkPage}){
 
-    return(
-        <article className="projects-form">
-            <img src={img} alt="project's screen" />
-            
-        </article>
-    )
+    return (
+			<article className="projects-form">
+				<div className="projects-form__emoji">{emoji}</div>
+				<h2 className="projects-form__title">{title}</h2>
+				<u className="projects-form__skills">
+					{skills.map((skill, index) => (
+						<li key={index} className="projects-form__skills__item">
+							{skill}
+						</li>
+					))}
+				</u>
+				<p className="projects-form__description">{description}</p>
+				<div className="projects-form__links">
+					{linkGit && (
+						<button className="projects-form__button">
+							<a href={linkGit}>
+								<FaGithub />
+								GitHub
+							</a>
+						</button>
+					)}
+					{linkPage && (
+						<button className="projects-form__button">
+							<a href={linkPage}>
+								<FaDesktop />
+								Page
+							</a>
+						</button>
+					)}
+				</div>
+			</article>
+		);
 
 }
--- src/components/Projets.jsx
+++ src/components/Projets.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import ProjectsForm from "./ProjectsForm";
+import ArgentBank from '../assets/ArgentBank.JPG';
+import '../styles/projects.css'
 export default function Projects(){
-    return(
-        <section className="projets">
-            <h1 className="projects__title title">Projects</h1>
-            <div className="projects__container">
-                <ProjectsForm />
-            </div>
-        </section>
-    )
+    return (
+			<section className="projets">
+				<h1 className="projects__title title">Projects</h1>
+				<div className="projects__container">
+					<ProjectsForm
+						emoji="💲"
+						title="Argent Bank"
+						description="Use an API for a bank user account with React"
+						skills={['React', 'Redux']}
+						linkGit="https://github.com/DrWhiskas/Argent-Bank-.git"
+					/>
+				</div>
+			</section>
+		);
 }
